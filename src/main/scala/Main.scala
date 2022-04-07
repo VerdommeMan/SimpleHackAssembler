037a5ac1616d4e7032bed9477c79892f306f72d3
VerdommeMan <verdommeman1717@gmail.com>
2022-04-08 00:18:38 +0200

    Before decline integration

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,11 +3,31 @@ import scala.sys.exit
 import scala.io.Source
 
 object Main extends App {
+  val helpMsg =
+    """
+      |This program is an assembler for the Hack assembly.
+      |
+      |USAGE:
+      | hackassembler path/to/input.asm [path/to/output.hack] [-verbose] [-force]
+      |
+      |FLAGS:
+      |   verbose: prints success and file written to
+      |   force: overwrite if output file already exists
+      |
+      |""".stripMargin
+
   if (args.length == 0) {
-    println("need one arg")
+    println(helpMsg)
     exit(1)
   }
-  val filename = args(0)
+
+  val config: Config = args.length match {
+    case 1 => Config(new File(args(0)))
+    case 2 => Config(new File(args(0)), new File(args(1)))
+    case 3 => Config(new File(args(0)), new File(args(1)), true)
+    case 4 => Config(new File(args(0)), new File(args(1)), true)
+    case _ => System.err.println("Wrong amount of arguments given!"); exit(1)
+  }
 
   val parser = new HackParser(HackLexer.tokenize(Source.fromFile(filename)))
 
